9e43b3e6e061af588cba71cd9cf696696daf14d7
Kutay YILDIRIM <kutay9622@gmail.com>
2021-04-05 22:26:26 +0300

    navbar updated

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,11 +1,13 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-
+import 'package:cool_alert/cool_alert.dart';
 import 'package:dodact_v1/ui/event/events_page.dart';
 import 'package:dodact_v1/ui/feed/feed_page.dart';
+import 'package:dodact_v1/ui/general/general_page.dart';
 import 'package:dodact_v1/ui/group/groups_page.dart';
 import 'package:dodact_v1/ui/profile/profile_page.dart';
 import 'package:dodact_v1/ui/search/search_page.dart';
+import 'package:extended_navbar_scaffold/extended_navbar_scaffold.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
 // Oturum açmış kullanıcıların görmesi gereken sayfa.
 
@@ -15,19 +17,142 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _page = 0;
+  int _page = 1;
   GlobalKey _bottomNavigationKey = GlobalKey();
   final List<Widget> _children = [
+    GeneralPage(),
     FeedPage(),
-    SearchPage(),
     EventsPage(),
     GroupsPage(),
     ProfilePage(),
+    SearchPage(),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return ExtendedNavigationBarScaffold(
+      body: _children[_page],
+      elevation: 0,
+      navBarColor: Colors.white,
+      navBarIconColor: Colors.black,
+      moreButtons: [
+        MoreButtonModel(
+          icon: MaterialCommunityIcons.earth,
+          label: 'Anasayfa',
+          onTap: () {
+            setState(() {
+              _page = 0;
+            });
+          },
+        ),
+        MoreButtonModel(
+          icon: MaterialCommunityIcons.instagram,
+          label: 'Akış',
+          onTap: () {
+            setState(() {
+              _page = 1;
+            });
+          },
+        ),
+        MoreButtonModel(
+          icon: MaterialCommunityIcons.air_horn,
+          label: 'Etkinlikler',
+          onTap: () {
+            setState(() {
+              _page = 2;
+            });
+          },
+        ),
+        MoreButtonModel(
+          icon: FontAwesome.group,
+          label: 'Gruplar',
+          onTap: () {
+            setState(() {
+              _page = 3;
+            });
+          },
+        ),
+        MoreButtonModel(
+          icon: MaterialCommunityIcons.calendar,
+          label: 'Takvimim',
+          onTap: () {
+            CoolAlert.show(
+              context: context,
+              type: CoolAlertType.loading,
+              text: "Bu özelliğimiz henüz aktif değil.",
+            );
+          },
+        ),
+        MoreButtonModel(
+          icon: FontAwesome5Regular.user_circle,
+          label: 'Profilim',
+          onTap: () {
+            setState(() {
+              _page = 4;
+            });
+          },
+        ),
+        null,
+        MoreButtonModel(
+          icon: Icons.settings,
+          label: 'Ayarlar',
+          onTap: () {},
+        ),
+        null,
+      ],
+      searchWidget: Container(
+        height: 50,
+        color: Colors.redAccent,
+      ),
+      // onTap: (button) {},
+      // currentBottomBarCenterPercent: (currentBottomBarParallexPercent) {},
+      // currentBottomBarMorePercent: (currentBottomBarMorePercent) {},
+      // currentBottomBarSearchPercent: (currentBottomBarSearchPercent) {},
+      parallexCardPageTransformer: PageTransformer(
+        pageViewBuilder: (context, visibilityResolver) {
+          return PageView.builder(
+            controller: PageController(viewportFraction: 0.85),
+            itemCount: parallaxCardItemsList.length,
+            itemBuilder: (context, index) {
+              final item = parallaxCardItemsList[index];
+              final pageVisibility =
+                  visibilityResolver.resolvePageVisibility(index);
+              return ParallaxCardsWidget(
+                item: item,
+                pageVisibility: pageVisibility,
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  final parallaxCardItemsList = <ParallaxCardItem>[
+    ParallaxCardItem(
+        title: 'Some Random Route 1',
+        body: 'Place 1',
+        background: Container(
+          color: Colors.orange,
+        )),
+    ParallaxCardItem(
+        title: 'Some Random Route 2',
+        body: 'Place 2',
+        background: Container(
+          color: Colors.redAccent,
+        )),
+    ParallaxCardItem(
+        title: 'Some Random Route 3',
+        body: 'Place 1',
+        background: Container(
+          color: Colors.blue,
+        )),
+  ];
+}
+
+/*
+
+Scaffold(
         bottomNavigationBar: CurvedNavigationBar(
           key: _bottomNavigationKey,
           index: 0,
@@ -52,39 +177,6 @@ class _HomePageState extends State<HomePage> {
           letIndexChange: (index) => true,
         ),
         body: _children[_page]);
-  }
-}
-
-//Yedek bottom bar
-/*
 
 
- */
-
-/*
-
-new Builder(
-            builder: (context) => new AnimatedBottomBar(
-                  defaultIconColor: Colors.black,
-                  activatedIconColor: Colors.redAccent,
-                  background: Colors.white,
-                  buttonsIcons: [
-                    Icons.home,
-                    Icons.search,
-                    Icons.group_work_rounded,
-                    Icons.person
-                  ],
-                  buttonsHiddenIcons: [
-                    Icons.camera_alt,
-                    Icons.videocam,
-                    Icons.mic,
-                    Icons.music_note
-                  ],
-                  backgroundColorMiddleIcon: Colors.purple,
-                  onTapButton: (i) {
-                    setState(() {
-                      _page = i;
-                    });
-                  },
-                ))
  */
