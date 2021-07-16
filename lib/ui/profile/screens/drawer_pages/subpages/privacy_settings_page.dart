20d738e85b7344329ebc81ac363630162f3363a6
Kutay YILDIRIM <kutay9622@gmail.com>
2021-07-16 14:44:45 +0300

    .

--- lib/ui/profile/screens/drawer_pages/subpages/privacy_settings_page.dart
+++ lib/ui/profile/screens/drawer_pages/subpages/privacy_settings_page.dart
@@ -1,11 +1,64 @@
 import 'package:flutter/material.dart';
 
-class PrivacySettingsPage extends StatelessWidget {
+class PrivacySettingsPage extends StatefulWidget {
+  @override
+  _PrivacySettingsPageState createState() => _PrivacySettingsPageState();
+}
+
+class _PrivacySettingsPageState extends State<PrivacySettingsPage> {
+  bool mailGizlilik = false;
+  bool ornekAyar2 = false;
+  bool ornekAyar3 = false;
+  bool ornekAyar4 = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Bildirim Ayarları"),
+        title: Text("Gizlilik Ayarları"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SwitchListTile(
+              title: Text('Mail Adresini Gizle'),
+              value: mailGizlilik,
+              onChanged: (value) {
+                setState(() {
+                  mailGizlilik = !mailGizlilik;
+                });
+              },
+            ),
+            SwitchListTile(
+              title: Text('Örnek Ayar'),
+              value: ornekAyar2,
+              onChanged: (value) {
+                setState(() {
+                  ornekAyar2 = !ornekAyar2;
+                });
+              },
+            ),
+            SwitchListTile(
+              title: Text('Örnek Ayar'),
+              value: ornekAyar3,
+              onChanged: (value) {
+                setState(() {
+                  ornekAyar3 = !ornekAyar3;
+                });
+              },
+            ),
+            SwitchListTile(
+              title: Text('Örnek Ayar'),
+              value: ornekAyar4,
+              onChanged: (value) {
+                setState(() {
+                  ornekAyar4 = !ornekAyar4;
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
