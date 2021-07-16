d50c518f976d9a956091603b3530ff68d93f750b
Kutay YILDIRIM <kutay9622@gmail.com>
2021-07-16 22:55:10 +0300

    discover page grids added

---
+++ lib/ui/discover/widgets/post_card_for_grids.dart
@@ -0,0 +1,38 @@
+import 'package:dodact_v1/common/methods.dart';
+import 'package:dodact_v1/config/constants/route_constants.dart';
+import 'package:dodact_v1/config/navigation/navigation_service.dart';
+import 'package:dodact_v1/model/post_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class PostCardForGrid extends StatelessWidget {
+  PostModel post;
+
+  PostCardForGrid({this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return postView(post);
+  }
+
+  Widget postView(PostModel post) {
+    var thumbnailURL = CommonMethods.createThumbnailURL(post.postContentURL);
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: InkWell(
+        onTap: () {
+          NavigationService.instance
+              .navigate(k_ROUTE_POST_DETAIL, args: post.postId);
+        },
+        child: Container(
+            padding: EdgeInsets.only(bottom: 10),
+            foregroundDecoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(thumbnailURL),
+              ),
+            )),
+      ),
+    );
+  }
+}
--- lib/ui/discover/widgets/posts_part.dart
+++ lib/ui/discover/widgets/posts_part.dart
@@ -1,61 +1,107 @@
 import 'package:dodact_v1/config/constants/theme_constants.dart';
 import 'package:dodact_v1/model/post_model.dart';
 import 'package:dodact_v1/provider/post_provider.dart';
-import 'package:dodact_v1/ui/discover/widgets/post_card.dart';
+import 'package:dodact_v1/ui/discover/widgets/post_card_for_grids.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:provider/provider.dart';
 
-class PostsPart extends StatelessWidget {
+class PostsPart extends StatefulWidget {
+  @override
+  _PostsPartState createState() => _PostsPartState();
+}
+
+class _PostsPartState extends State<PostsPart> {
+  Future _postsFuture;
+
+  Future _refreshPostsFuture() {
+    return Provider.of<PostProvider>(context, listen: false).getList();
+  }
+
+  Future<void> _refreshPosts() async {
+    await Provider.of<PostProvider>(context, listen: false).getList();
+  }
+
+  @override
+  void initState() {
+    _postsFuture = _refreshPostsFuture();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final postProvider = Provider.of<PostProvider>(context, listen: false);
 
     return SafeArea(
-      child: FutureBuilder(
-        future: postProvider.getList(),
-        // ignore: missing_return
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.active:
-            case ConnectionState.waiting:
-              return Center(
-                child: spinkit,
-              );
-            case ConnectionState.done:
-              if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              }
-              List<PostModel> posts = snapshot.data;
-              return Column(
-                children: [
-                  // StoryContainer(),
-                  Expanded(
-                    child: ListView.builder(
-                      padding: EdgeInsets.zero,
-                      primary: false,
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      itemCount: posts.length,
-                      itemBuilder: (context, index) {
-                        // provider.postList.shuffle();
-                        var postItem = posts[index];
-                        return Container(
-                          height: 400,
-                          child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: PostCard(
-                              post: postItem,
-                            ),
-                          ),
-                        );
-                      },
-                    ),
+      child: RefreshIndicator(
+        onRefresh: () => _refreshPosts(),
+        child: FutureBuilder(
+          future: _postsFuture,
+          // ignore: missing_return
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.active:
+              case ConnectionState.waiting:
+                return Center(
+                  child: spinkit,
+                );
+              case ConnectionState.done:
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
+                List<PostModel> posts = snapshot.data;
+
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: StaggeredGridView.countBuilder(
+                    crossAxisCount: 4,
+                    itemCount: posts.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      var postItem = posts[index];
+                      return Container(
+                        height: 400,
+                        child: PostCardForGrid(
+                          post: postItem,
+                        ),
+                      );
+                    },
+                    staggeredTileBuilder: (int index) =>
+                        new StaggeredTile.count(2, index.isEven ? 2 : 1),
+                    mainAxisSpacing: 4.0,
+                    crossAxisSpacing: 4.0,
                   ),
-                ],
-              );
-          }
-        },
+                );
+              // return Column(
+              //   children: [
+              //     // StoryContainer(),
+              //     Expanded(
+              //       child: ListView.builder(
+              //         padding: EdgeInsets.zero,
+              //         primary: false,
+              //         scrollDirection: Axis.vertical,
+              //         shrinkWrap: true,
+              //         itemCount: posts.length,
+              //         itemBuilder: (context, index) {
+              //           // provider.postList.shuffle();
+              //           var postItem = posts[index];
+              // return Container(
+              //   height: 400,
+              //   child: Padding(
+              //     padding: const EdgeInsets.all(5.0),
+              //     child: PostCard(
+              //       post: postItem,
+              //     ),
+              //   ),
+              // );
+              //         },
+              //       ),
+              //     ),
+              //   ],
+              // );
+            }
+          },
+        ),
       ),
     );
   }
