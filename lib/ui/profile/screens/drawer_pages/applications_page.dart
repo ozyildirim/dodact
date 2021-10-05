02a21933c8c4a54afe286382eb4338aac1e0bec0
Burak Kızılay <40871181+burakkizilay46@users.noreply.github.com>
2021-10-05 11:31:55 +0300

    form sayfası eklendi!

--- lib/ui/profile/screens/drawer_pages/applications_page.dart
+++ lib/ui/profile/screens/drawer_pages/applications_page.dart
@@ -1,3 +1,4 @@
+import 'package:dodact_v1/config/constants/theme_constants.dart';
 import 'package:flutter/material.dart';
 
 class ApplicationsPage extends StatefulWidget {
@@ -9,9 +10,72 @@ class _ApplicationsPageState extends State<ApplicationsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("asdsadd"),
-      ),
-    );
+        backgroundColor: Color(0xff483D8B),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          centerTitle: true,
+          title: Text("BAŞVUR"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(right: 6, left: 6),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(24),
+                    child: Image(
+                        image: NetworkImage(
+                            "https://ae01.alicdn.com/kf/HTB1U7dDX6LuK1Rjy0Fhq6xpdFXad.jpg")),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.fromLTRB(12, 325, 12, 12),
+                      child: Text(
+                        "BAŞVURU YAPMAK İÇİN",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(12),
+                      child: Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.85,
+              height: MediaQuery.of(context).size.height * 0.07,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: Colors.white),
+                onPressed: () {},
+                child: Text(
+                  "BAŞVURU YAP",
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
